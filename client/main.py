0458baa7d50f9322cebc0074d7ade1ea42b31708
Vladan Trhlík <vladantrhlik@gmail.com>
2024-09-19 18:38:19 +0200

    basic data and view, will probably move to pygame

--- client/main.py
+++ client/main.py
@@ -1,9 +1,25 @@
-
 from pyray import *
-init_window(800, 450, "Hello")
+from game_data import GameData, Player
+from game_view import GameView
+
+
+gd = GameData(3, 3)
+gd.set_stick(0, 0, Player.ME)
+gd.set_stick(1, 1, Player.HIM)
+gd.set_stick(2, 1, Player.HIM)
+gd.set_stick(3, 1, Player.ME)
+
+gv = GameView(gd)
+
+
+init_window(600, 600, "Dots and Boxes")
+
 while not window_should_close():
     begin_drawing()
+    
     clear_background(WHITE)
-    draw_text("Hello world", 190, 200, 20, VIOLET)
+    gv.draw()
+
     end_drawing()
 close_window()
+
