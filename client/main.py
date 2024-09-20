598d55b7835ce21c1cfd275f5717b75c1d1275b1
Vladan Trhlík <vladantrhlik@gmail.com>
2024-09-20 14:03:14 +0200

    basic scene, login scene

--- client/main.py
+++ client/main.py
@@ -2,30 +2,32 @@ import pygame as pg
 from game_data import GameData, Player
 from game_view import GameView
 from colors import Col
-
-
-gd = GameData(3, 3)
-gd.set_stick(0, 0, Player.ME)
-gd.set_stick(1, 1, Player.HIM)
-gd.set_stick(2, 1, Player.HIM)
-gd.set_stick(3, 1, Player.ME)
-
-gv = GameView(gd)
+from scenes import Scene, LoginScene
 
 pg.init()
 screen = pg.display.set_mode([500, 500], pg.RESIZABLE)
+clock = pg.time.Clock()
 
-# Run until the user asks to quit
+# start game with login screen
+curr_scene: Scene = LoginScene({})
+
 running = True
 while running:
+    delta_time = clock.tick(60)/1000.0
+
+    # process all events
     for event in pg.event.get():
         if event.type == pg.QUIT:
             running = False
+        curr_scene.process_event(event)
 
-    # Fill the background with white
+    # update current scene
+    curr_scene.update(delta_time)
+
+    # draw current scene
     screen.fill(Col.BACKGROUND)
-    gv.draw(screen)
+    curr_scene.draw(screen)
+
     pg.display.flip()
 
-# Done! Time to quit.
 pg.quit()
