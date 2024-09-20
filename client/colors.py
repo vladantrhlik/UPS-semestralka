a8b1e873ff1a699bced87d153261efa53b740116
Vladan Trhlík <vladantrhlik@gmail.com>
2024-09-20 07:30:03 +0200

    migration to pygame

---
+++ client/colors.py
@@ -0,0 +1,7 @@
+from enum import Enum
+
+class Col:
+    PLAYER = (0,0,255)
+    ENEMY = (255,0,0)
+    BACKGROUND = [232]*3
+    DOT = (0,0,0)
--- client/game_view.py
+++ client/game_view.py
@@ -1,21 +1,29 @@
 from game_data import *
-from pyray import *
+import pygame as pg
+from pygame import gfxdraw
+from colors import Col
 
 class GameView:
     def __init__(self, game_data: GameData):
         self.game_data = game_data
-        self.min_padding = 20
-        self.dot_radius = 10
 
+        self.min_padding = 20
+        self.dot_radius = 7
+        self.stick_width = 4
+
+        '''
         # set window resizeable
         set_config_flags(FLAG_WINDOW_RESIZABLE)
         # aliasing
         set_config_flags(FLAG_MSAA_4X_HINT)
+        '''
+    def draw_circle(self, surface, x, y, radius, color):
+        gfxdraw.aacircle(surface, x, y, radius, color)
+        gfxdraw.filled_circle(surface, x, y, radius, color)
 
-    def draw(self):
+    def draw(self, screen):
         # screen size
-        w = get_screen_width()
-        h = get_screen_height()
+        w,h = pg.display.get_window_size()
 
         padding = max(min(w, h) * 0.05, self.min_padding)
 
@@ -30,24 +38,25 @@ class GameView:
         for y in range(len(self.game_data.stick_data)):
             for x in range(len(self.game_data.stick_data[y])):
                 val = self.game_data.stick_data[y][x]
-
-                col = BLANK 
-                if val == Player.HIM: col = RED
-                if val == Player.ME: col = BLUE
+                if val == Player.NONE: continue
+                col = Col.ENEMY if val == Player.HIM else Col.PLAYER
 
                 if y % 2 == 0:
                     # horizontal
-                    draw_line(off_x + x * tile, off_y + (y // 2) * tile,
-                              off_x + (x+1) * tile, off_y + (y // 2) * tile, col)
+                    pg.draw.line(screen, col,
+                                 (off_x + x * tile, off_y + (y // 2) * tile),
+                                 (off_x + (x+1) * tile, off_y + (y // 2) * tile),
+                                 width = self.stick_width)
                 else:
                     # vertical
-                    draw_line(off_x + x * tile, off_y + (y // 2) * tile,
-                              off_x + x * tile, off_y + (y // 2 + 1) * tile, col)
-
-
-
+                    pg.draw.line(screen, col,
+                                 (off_x + x * tile, off_y + (y // 2) * tile),
+                                 (off_x + x * tile, off_y + (y // 2 + 1) * tile),
+                                 width = self.stick_width)
 
         # draw dots
         for y in range(self.game_data.h + 1):
             for x in range(self.game_data.w + 1):
-                draw_circle(off_x + x * tile, off_y + y * tile, self.dot_radius, BLACK)
+                # pg.draw.circle(screen, (0,0,0),
+                #            (off_x + x * tile, off_y + y * tile), self.dot_radius)
+                self.draw_circle(screen, off_x + x * tile, off_y + y * tile, self.dot_radius, Col.DOT)
