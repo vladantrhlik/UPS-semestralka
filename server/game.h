8c99f963e31df2813100f146db8c4daded9abe3e
Vladan Trhlík <vladantrhlik@gmail.com>
2024-11-01 00:56:46 +0100

    info about acquiring squares

--- server/game.h
+++ server/game.h
@@ -9,7 +9,7 @@ int is_game_finished(Game *g);
 
 Game *game_create(int width, int height, char *name, Player *p0);
 
-int game_set(Game *g, int player, int x, int y);
+int *game_set(Game *g, int player, int x, int y);
 
 void game_free(Game *g);
 
